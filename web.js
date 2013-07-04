cdf365db45df65c7a40315c399d2c9fb126d71d4
XU Jingwei <paragon@XUJingwei.local>
2013-07-04 23:00:56 +0800

    update index.html

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
+var fs = require('fs');
+
 var express = require('express');
+
+
+var fileName = "index.html";
+var content = fs.readFileSync(fileName, "utf-8");
+
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer(content,"utf-8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
